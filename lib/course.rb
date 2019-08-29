85ca61316ebc8e60f90ae82e72ca7b9953c92b34
Alouiscious Media <info@reneesoul.com>
2019-08-28 17:11:16 -0700

    Update course.rb

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,24 @@
 
 
+class Course
+    attr_accessor :title, :schedule, :description
+
+    def self.all
+        @@all =[]
+        @all << self
+    end
+
+    def title
+        @title = title
+    end
+
+    def schedule
+        @schedule = schedule
+    end
+
+    def description
+        @description = description
+    end
+
+end
+
